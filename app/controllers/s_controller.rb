89dff2af560bf54cabd2624658026d153d144cc1
allen <allen@seravia.com>
2010-07-24 18:03:48 +0800

    fix short url controller

--- app/controllers/s_controller.rb
+++ app/controllers/s_controller.rb
@@ -3,5 +3,10 @@ class SController < ApplicationController
     id = params[:id]
     entry_id = Base52.decode(id,SHORT_URL_OFFSET)
     @entry = Entries::Base.find_by_id(entry_id)
+    unless @entry
+      render :status => 404 
+    else 
+      @entry
+    end
   end
 end
--- spec/controllers/s_controller_spec.rb
+++ spec/controllers/s_controller_spec.rb
@@ -2,9 +2,22 @@ require 'spec_helper'
 
 describe SController do
 
-  #Delete this example and add some real ones
-  it "should use SController" do
-    controller.should be_an_instance_of(SController)
-  end
+  describe "#index" do 
+    it "should render view" do
+      Base52.should_receive(:decode).with("abc",SHORT_URL_OFFSET).and_return(1)
+      entry = Entries::Base.new
+      Entries::Base.should_receive(:find_by_id).with(1).and_return(entry)
+      get :index, :id => "abc"
+      response.should render_template "index"
+      response.should be_success
+      assigns(:entry).should == entry
+    end
 
+    it "should return 404 if entry donesn't exist" do
+      Base52.should_receive(:decode).with("abc",SHORT_URL_OFFSET).and_return(1)
+      Entries::Base.should_receive(:find_by_id).with(1).and_return(false)
+      get :index, :id => "abc"
+      response.response_code.should == 404
+    end
+  end
 end

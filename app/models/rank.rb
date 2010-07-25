862a3245cf8b2d0b51e91e0215eba34b0eeb75e8
allen <allen@seravia.com>
2010-07-25 08:54:42 +0800

    test mongo_mapper

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,7 +1,14 @@
 class Rank 
-  include Mongoid::Document
-  field :record_id, :type => Integer
-  field :rank, :type => Integer
-  field :comment, :type => Integer
-  field :date, :type => Date
+#  include Mongoid::Document
+  #field :record_id, :type => Integer
+  #field :rank, :type => Integer
+  #field :comment, :type => Integer
+  #field :date, :type => Date
+  include MongoMapper::Document
+
+  key :record_id, :type => Integer
+  key :rank, :type => Integer
+  key :comment, :type => Integer
+  key :date, :type => Date
+
 end
